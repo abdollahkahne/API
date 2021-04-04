cc48728e80df73b79317e6ba529a7c197043b2dd
abdollahkahne <abdollahkahne@gmail.com>
2021-04-05 02:04:13 +0430

    Remove Cors middleware too

--- routes/auth.routh.js
+++ routes/auth.routh.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors");
+// const cors = require("cors");
 const controller = require("../controllers/auth.controller");
 
 const router = express.Router();
@@ -8,10 +8,10 @@ router.use(bodyParser.json());
 // router.use(express.urlencoded({ extended: true }));
 // router.use(express.json());
 
-router.use(cors({
-  credentials: true,
-  origin: process.env.UI_SERVER_ORIGIN,
-}));
+// router.use(cors({
+//   credentials: true,
+//   origin: process.env.UI_SERVER_ORIGIN,
+// }));
 
 router.post("/signin", controller.verifyLogin);
 router.post("/signout", controller.signOut);
